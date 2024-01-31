9a375a0fd8c7ec91d3518223f6f96e58d7ac2efa
radonnachie <code@radonn.co.za>
2024-01-31 08:41:24 +0200

    * version based on tags

--- src/blri/__init__.py
+++ src/blri/__init__.py
@@ -1,9 +1,8 @@
 import os
 import logging
+from ._version import __version__
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.WARNING)
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), 'r') as fio:
-    __version__ = fio.read()
