e83836d4d1a2f0b2202396d047310087035a3288
radonnachie <code@radonn.co.za>
2024-03-28 08:11:21 +0200

    ^ entrypoints/pycorr standardise timespan_s

--- src/blri/entrypoints/pycorr.py
+++ src/blri/entrypoints/pycorr.py
@@ -35,7 +35,7 @@ class InputGuppiIterator:
         nof_spectra_per_block: int
         nof_packet_indices_per_block: int
         sample_packet_index: int
-        spectra_timespan: float
+        spectra_timespan_s: float
         time_unix_offset: float
     
     def __init__(self, raw_filepaths: List[str], dtype="float32", unsorted_raw_filepaths=False):
@@ -52,7 +52,7 @@ class InputGuppiIterator:
             nof_spectra_per_block = self.guppi_header.nof_spectra_per_block,
             nof_packet_indices_per_block = self.guppi_header.nof_packet_indices_per_block,
             sample_packet_index = self.guppi_header.packet_index,
-            spectra_timespan = self.guppi_header.spectra_timespan,
+            spectra_timespan_s = self.guppi_header.spectra_timespan_s,
             time_unix_offset = self.guppi_header.time_unix_offset
         )
 
@@ -78,7 +78,7 @@ class InputGuppiIterator:
             phase_center_rightascension_radians = parse.degrees_process(self.guppi_header.get("RA_PHAS", self.guppi_header.rightascension_string)) * numpy.pi / 12.0,
             phase_center_declination_radians = parse.degrees_process(self.guppi_header.get("DEC_PHAS", self.guppi_header.declination_string)) * numpy.pi / 180.0,
             dut1_s = self.guppi_header.get("DUT1", 0.0),
-            spectra_timespan_s = self.guppi_header.spectra_timespan,
+            spectra_timespan_s = self.guppi_header.spectra_timespan_s,
             telescope = self.guppi_header.telescope,
             source_name = self.guppi_header.source_name,
             antenna_names = self.guppi_header.antenna_names if hasattr(self.guppi_header, "antenna_names") else None
@@ -108,7 +108,7 @@ class InputGuppiIterator:
         unix_midpoint = self.timekeeper.time_unix_offset + (
             (self.timekeeper.sample_packet_index + packet_index_step/2)
             * (self.timekeeper.nof_spectra_per_block/self.timekeeper.nof_packet_indices_per_block)
-        ) * self.timekeeper.spectra_timespan
+        ) * self.timekeeper.spectra_timespan_s
 
         self.timekeeper.sample_packet_index += packet_index_step
         return unix_midpoint
@@ -126,7 +126,7 @@ class InputGuppiIterator:
 
 class InputStampIterator:
     class StampTimeKeeper(BaseModel):
-        spectra_timespan: float
+        spectra_timespan_s: float
         time_unix_offset: float
         running_time_unix: float
     
@@ -145,7 +145,7 @@ class InputStampIterator:
             raise RuntimeError(f"Did not reach index {stamp_index} within {self.stamp_filepath}")
 
         self.timekeeper = self.StampTimeKeeper(
-            spectra_timespan = stamp.tsamp,
+            spectra_timespan_s = stamp.tsamp,
             time_unix_offset = stamp.tstart,
             running_time_unix = 0
         )
@@ -163,8 +163,7 @@ class InputStampIterator:
         blri_logger.debug(f"Total Stamp Data bytes: {self.stamp_bytes_total/(10**6)} MB")
     
     def metadata(self, polarisation_chars=None) -> MetaData:
-        if polarisation_chars is None:
-            polarisation_chars = "ab"
+        assert polarisation_chars is not None
         return MetaData(
             nof_antenna = self.stamp.numAntennas,
             nof_channel = self.stamp.numChannels,
@@ -205,7 +204,7 @@ class InputStampIterator:
         self._data_bytes_processed = self.stamp_bytes_total
 
     def increment_time_taking_midpoint_unix(self, step_timesamples) -> float:
-        time_step = step_timesamples*self.spectra_timespan
+        time_step = step_timesamples*self.spectra_timespan_s
         unix_midpoint = self.time_unix_offset + self.running_time_unix + time_step/2
         self.running_time_unix += time_step
         return unix_midpoint
